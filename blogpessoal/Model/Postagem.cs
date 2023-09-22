c6ac082439dcd6546ca7ef4b35e1be63bad78b14
apaixav <lucasrib01@gmail.com>
2023-09-22 16:35:47 -0300

    ASP.NET

---
+++ blogpessoal/Model/Postagem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace blogpessoal.Model
+{
+    public class Postagem : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(100)]
+        public string titulo { get; set; } = string.Empty;
+
+        [Column(TypeName = "varchar")]
+        [StringLength(1000)]
+        public string texto { get; set; } = string.Empty;
+    }
+}
---
+++ blogpessoal/Validator/PostagemValidator.cs
@@ -0,0 +1,21 @@
+﻿using blogpessoal.Model;
+using FluentValidation;
+
+namespace blogpessoal.Validator
+{
+    public class PostagemValidator : AbstractValidator<Postagem>
+    {
+        public PostagemValidator() {
+
+            RuleFor(p => p.titulo)
+                .NotEmpty()
+                .MinimumLength(5)
+                .MaximumLength(100);
+
+            RuleFor(p => p.texto)
+                .NotEmpty()
+                .MinimumLength(10)
+                .MaximumLength(1000);
+        }
+    }
+}
